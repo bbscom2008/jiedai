f5aad800338e9575cc3c48ba49c24a113785a008
haohao <haohao@qq.com>
2016-11-17 14:42:08 +0800

    1

--- app/src/main/java/com/jiedai/MainActivity.java
+++ app/src/main/java/com/jiedai/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 我先写入一些内容，提交试试看，
+
     }
 }
