1075a74c841dcd825a20c0e5f85b2746d7f7b457
wangxt1991 <747788296@qq.com>
2016-11-17 16:35:00 +0800

    创建了一个新的类

--- app/src/main/java/com/jiedai/HomeActivity.java
+++ app/src/main/java/com/jiedai/HomeActivity.java
@@ -3,14 +3,11 @@ package com.jiedai;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // 我先写入一些内容，提交试试看，
-
-
+        setContentView(R.layout.activity_home);
     }
 }
